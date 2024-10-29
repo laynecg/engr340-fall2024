21b8538195612d356645aaf47ef9039698c31cb7
MINICPR <laynecg@dukes.jmu.edu>
2024-10-29 11:59:30 -0400

    Commit - 10/29

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,3 +1,4 @@
+from array import array
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,13 +7,20 @@ import numpy as np
 path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
-
+data = np.loadtxt(path, delimiter=',', skiprows=2)
 ### Your code here ###
 
 # save each vector as own variable
-
+elapsed_time = data[:,0]
+mlii = data[:,1]
+v1 = data[:,2]
 ### Your code here ###
 
-# use matplot lib to generate a single
+# use matplot lib to generate a single plot
+plt.plot(elapsed_time, mlii, v1, label='EKG Data', color='purple')
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (mV)')
+plt.title('Heartbeat EKG')
+plt.show()
 
 ### Your code here ###
